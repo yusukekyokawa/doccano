49a55a4972cd9cadc03725e5213b745858c639e4
yusukekyokawa <medakasuisui598@gmail.com>
2020-09-29 18:03:03 +0900

    check access for localhost using docker-compose.dev.yml

--- app/app/settings.py
+++ app/app/settings.py
@@ -344,5 +344,6 @@ if DEBUG:
     CORS_ORIGIN_WHITELIST = (
         'http://127.0.0.1:3000',
         'http://0.0.0.0:3000',
-        'http://localhost:3000'
+        'http://localhost:3000',
+        'https://localhost'
     )
